1bd379282a581d591372040ee6c597c2016fa61c
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2019-04-17 10:18:05 +0300

    remove key

--- plugins/echo.js
+++ plugins/echo.js
@@ -3,7 +3,7 @@ window.Pusher = require('pusher-js');
 export default ({ app }, inject) => {
   inject('echo', new Echo({
     broadcaster: 'pusher',
-    key: 'a46368bb79cc0e5c003b',
+    key: '',
     cluster: 'eu',
     encrypted: true
   }));
